ee0b698217f60590b0a3a8b110cfa6a3c4f14a6a
Jonathan Gamez <jgamez857@hotmail.es>
2025-10-21 18:51:27 -0500

    aplicacion  con docker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect( () => {
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then(response =>{
+        setCharacters(response.data.results);
+      })
+      .catch(error => console.error('Error:', error));
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Personajes de rick y morty</h1>
+      {characters.map(character => (
+        <div key={character.id}>
+          <h2> {character.name} </h2>
+          <p>Especie: {character.species}</p>
+          <img src={character.image} alt={character.name} />
+        </div>
+      ))}
+
     </div>
   );
 }
